3f3a50c27313d66a00c66cf418bcf1d14ae8cd36
volkovMikhail <volkov.work1@gmail.com>
2024-06-09 02:07:39 +0300

    fix: add engine info, change port env variable

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const cors = require('cors');
 
 //all env vars
-const port = process.env.HTTP_PORT ?? 3000;
+const port = process.env.PORT ?? 3000;
 const baseDomain = process.env.BASE_DOMAIN;
 const mongodbUri = process.env.MONGODB_CONNECTION;
 
